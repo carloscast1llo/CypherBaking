c95225fdf4d2bbcbe40136b42e87bb9f57bf8ff1
Lucas R. Luque <lucasrluque@icloud.com>
2021-10-19 00:55:30 +0200

    base64 added

--- cypherbaking/main.py
+++ cypherbaking/main.py
@@ -1,4 +1,23 @@
+import base64
+
+
 def menu():
+    print("*********************************************************************")
+    print("""
+                       _               _           _    _             
+                      | |             | |         | |  (_)            
+       ___ _   _ _ __ | |__   ___ _ __| |__   __ _| | ___ _ __   __ _ 
+      / __| | | | '_ \| '_ \ / _ \ '__| '_ \ / _` | |/ / | '_ \ / _` |
+     | (__| |_| | |_) | | | |  __/ |  | |_) | (_| |   <| | | | | (_| |
+      \___|\__, | .__/|_| |_|\___|_|  |_.__/ \__,_|_|\_\_|_| |_|\__, |
+            __/ | |                                              __/ |
+           |___/|_|                                             |___/ 
+
+    """)
+    print("  CypherBaking v1.0")
+    print("  La Panaderia")
+    print("  https://github.com/carloscast1llo/CypherBaking")
+    print("*********************************************************************")
     print("Welcome to Cyberbaking")
     menu = True
 
@@ -24,29 +43,42 @@ def menu():
             print("Invalid input")
 
 
+def toBase64():
+    text = input("Write the text you want to encrypt: ")
+    text_bytes = text.encode('ascii')
+    base64_bytes = base64.b64encode(text_bytes)
+    base64_text = base64_bytes.decode('ascii')
+    print(base64_text)
+    cont = input("Press ENTER to continue")
+    menu()
+
+
+def fromBase64():
+    base64_text = input("Write the text you want to decrypt: ")
+    base64_bytes = base64_text.encode('ascii')
+    text_bytes = base64.b64decode(base64_bytes)
+    decrypted_text = text_bytes.decode('ascii')
+    print(decrypted_text)
+    cont = input("Press ENTER to continue")
+    menu()
+
+
 def encryptMessage():
-    print("Encrypt")
+    print("Which type of encryption you want to use: ")
+    print("1. Base 64")
+    option = input()
+
+    if option == "1":
+        toBase64()
 
 
 def decryptMessage():
-    print("Decrypt")
+    print("Which type of encryption you want to use: ")
+    print("1. Base 64")
+    option = input()
 
+    if option == "1":
+        fromBase64()
 
-print("*********************************************************************")
-print("""
-                   _               _           _    _             
-                  | |             | |         | |  (_)            
-   ___ _   _ _ __ | |__   ___ _ __| |__   __ _| | ___ _ __   __ _ 
-  / __| | | | '_ \| '_ \ / _ \ '__| '_ \ / _` | |/ / | '_ \ / _` |
- | (__| |_| | |_) | | | |  __/ |  | |_) | (_| |   <| | | | | (_| |
-  \___|\__, | .__/|_| |_|\___|_|  |_.__/ \__,_|_|\_\_|_| |_|\__, |
-        __/ | |                                              __/ |
-       |___/|_|                                             |___/ 
-
-""")
-print("  CypherBaking v1.0")
-print("  La Panaderia")
-print("  https://github.com/carloscast1llo/CypherBaking")
-print("*********************************************************************")
 
 menu()
